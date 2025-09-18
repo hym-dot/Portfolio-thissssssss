684a3e9c0a38ae72a23fddd5c714637b27dcdb98
hym-dot <yumin6350@kbu.ac.kr>
2025-09-18 18:09:43 +0900

    .

--- frontend/src/components/admin/AdminContact.jsx
+++ frontend/src/components/admin/AdminContact.jsx
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from "react";
-import "./styles/AdminContact.scss";
-import { api } from "../../lib/api";
-import Swal from "sweetalert2";
+import React, { useState, useEffect } from 'react'
+import "./styles/AdminContact.scss"
+import { api } from '../../lib/api'
+import Swal from 'sweetalert2'
 const AdminContact = () => {
-  const [contacts, setContacts] = useState([]);
+
+  const [contacts, setContacts] = useState([])
 
   useEffect(() => {
+
     const fetchData = async () => {
       try {
-        const res = await api.get("/api/contact");
-        setContacts(res.data);
-      } catch (error) {
-        console.log("문의글 가져오기 실패", error);
-      }
-    };
+        const res = await api.get('/api/contact')
+        setContacts(res.data)
 
-    fetchData();
-  }, []);
+      } catch (error) {
+        console.log('문의글 가져오기 실패', error)
+      }
+    }
+
+    fetchData()
+
+  }, [])
+
 
   const handleStatusUpdate = async (contactId, newStatus) => {
     try {
@@ -24,19 +29,19 @@ const AdminContact = () => {
         `/api/contact/${contactId}`,
         { status: newStatus },
         { withCredentials: true }
-      );
+      )
 
       setContacts((prev) =>
         prev.map((c) => (c._id === contactId ? { ...c, status: newStatus } : c))
-      );
-      Swal.fire("수정완료", "상태가 성공적으로 수정되었습니다.", "success");
+      )
+      Swal.fire("수정완료", "상태가 성공적으로 수정되었습니다.", "success")
     } catch (error) {
-      console.log("수정실패", error);
-      Swal.fire("오류발생", "수정중 문제가 발생했습니다.", "error");
+      console.log("수정실패", error)
+      Swal.fire("오류발생", "수정중 문제가 발생했습니다.", "error")
     }
-  };
+  }
 
-    const showStatusChangeModal = async (contact) => {
+  const showStatusChangeModal = async (contact) => {
     const { value: newStatus, isConfirmed } = await Swal.fire({
       title: "문의 상태 수정",
       input: "radio",
@@ -50,13 +55,13 @@ const AdminContact = () => {
       cancelButtonText: "취소",
       showCancelButton: true
     })
-    if(isConfirmed&&newStatus){
-      handleStatusUpdate(contact._id,newStatus)
+    if (isConfirmed && newStatus) {
+      handleStatusUpdate(contact._id, newStatus)
     }
 
   }
 
-    const handleDelete = async (id) => {
+  const handleDelete = async (id) => {
     const result = await Swal.fire({
       title: "삭제하시겠습니까?",
       text: "이 작업은 되돌릴 수 없습니다.",
@@ -68,61 +73,67 @@ const AdminContact = () => {
       cancelButtonText: "취소",
     })
 
-    if(!result.isConfirmed) return;
+    if (!result.isConfirmed) return;
 
     try {
-      await api.delete(`/api/contact/${id}`,{
-        withCredentials:true
+      await api.delete(`/api/contact/${id}`, {
+        withCredentials: true
       })
 
-      setContacts((prev)=>prev.filter((c)=>c._id !==id))
-      Swal.fire("삭제완료","문의가 성공적으로 삭제 되었습니다.","success")
+      setContacts((prev) => prev.filter((c) => c._id !== id))
+      Swal.fire("삭제완료", "문의가 성공적으로 삭제 되었습니다.", "success")
     } catch (error) {
-      console.log("삭제 실패",error)
-      Swal.fire("오류발생","삭제 중 문제가 발생했습니다.","error")
+      console.log("삭제 실패", error)
+      Swal.fire("오류발생", "삭제 중 문제가 발생했습니다.", "error")
     }
 
   }
-
-
-
   return (
-    <div className="inner admin-contact">
+    <div className='inner admin-contact'>
       <h2>문의글 관리</h2>
       <div className="contact-wrapper">
         <ul className="contact-list">
           {contacts.map((c) => (
+
             <li key={c._id}>
               <p>
-                <span className="key">이름</span>:
-                <span className="value">{c.name}</span>
+                <span className='key'>
+                  이름
+                </span>
+                :
+                <span className='value'>
+                  {c.name}
+                </span>
               </p>
               <p>
-                <span className="key">email</span>:
-                <span className="value">{c.email}</span>
+                <span className='key'>email</span>
+                :
+                <span className='value'>
+                  {c.email}
+                </span>
               </p>
               <p>
-                <span className="key">전화번호</span>:
-                <span className="value">{c.phone}</span>
+                <span className='key'>전화번호</span>:
+                <span className='value'>
+                  {c.phone}
+                </span>
+              </p>
+              <p><span className='key'>메세지</span></p>
+              <p className='message'>
+                {c.message}
               </p>
               <p>
-                <span className="key">메세지</span>
-              </p>
-              <p className="message">{c.message}</p>
-              <p>
-                <span className="key">상태</span>
-                <span
-                  className={`status s-${(c.status || "").replace(" ", "-")}`}
-                >
+                <span className='key'>상태</span>
+                <span className={`status s-${(c.status || "").replace(" ", "-")}`}>
                   {c.status}
                 </span>
               </p>
               <div className="btns">
                 <button
-                onClick={()=>showStatusChangeModal(c)}
+                  onClick={() => showStatusChangeModal(c)}
                 >상태변경</button>
                 <button
-                onClick={()=>handleDelete(c._id)}
+                  onClick={() => handleDelete(c._id)}
                 >삭제</button>
               </div>
             </li>
@@ -130,7 +141,7 @@ const AdminContact = () => {
         </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AdminContact;
+export default AdminContact
